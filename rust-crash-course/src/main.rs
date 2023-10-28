f37188249375f3021619446d000cefa21b95082c
calnix <calnix.289@gmail.com>
2023-10-29 06:24:01 +0800

    structs

--- rust-crash-course/src/main.rs
+++ rust-crash-course/src/main.rs
@@ -1,5 +1,18 @@
 #![deny(clippy::all)]
 
-fn main() {
-    println!("Hello, world");
+struct Person {
+    name: String,
+    age: u8,
 }
+
+
+fn main() {
+ 
+    let person = Person {
+        age: 30,
+        name: "John".to_string(),
+    };
+
+    println!("{}", person.age)
+}
+
---
+++ rust-crash-course/target/debug/deps/rust_crash_course-af10b3f41f96201b.d
@@ -0,0 +1,5 @@
+C:\Users\PC\Desktop\rust-tutorial\rust-crash-course\target\debug\deps\rust_crash_course-af10b3f41f96201b.rmeta: src\main.rs
+
+C:\Users\PC\Desktop\rust-tutorial\rust-crash-course\target\debug\deps\rust_crash_course-af10b3f41f96201b.d: src\main.rs
+
+src\main.rs:
